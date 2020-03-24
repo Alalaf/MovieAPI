a9775a6cbc4bcc0bf49b8c78c51b82a92d04d4cc
ahmedalalaf <ahmedalalaf@gmail.com>
2020-03-25 00:33:29 +0300

    Add DbContext Class

---
+++ MovieAPI/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAPI
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext([NotNullAttribute]DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
